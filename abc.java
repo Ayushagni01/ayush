622978df060b08c860945302a9f9e4d4904f5595
Ayushagni01 <ayush.ayush24@gmail.com>
2018-10-25 07:15:18 +0530

    frst commit ever to be launched

---
+++ abc.java
@@ -0,0 +1,19 @@
+	class abc{
+	
+	    public static void main(String args[]){
+	        System.out.println("I think being a programmer is cool");
+	        System.out.println("This is how you can print the text in the code editors");
+	        System.out.println("\n");
+	        System.out.print("You can check the reqiured result in new line ");
+	       for (int x = 2; x <= 4; x++) {
+            System.out.println("Value of x:" + x); 
+    } 
+	        for (int y=1; y <= 5; y++) {
+            System.out.println("Ayush Agniotri is the best inthe world "); 
+            Syteml.out.println("https://www.geeksforgeeks.org/practice-for-cracking-any-coding-interview/
+https://www.ibpsguide.com/data-interpretation-pdf");
+    } 
+	    
+	    
+	}
+}
